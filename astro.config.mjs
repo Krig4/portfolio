e573105b9567cb8074119394d68399002c954688
alberto <albertoguerra4@gmail.com>
2025-03-27 19:15:09 +0100

    BasePortfolioWeb

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel/serverless';
 
 
+import react from "@astrojs/react";
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: 'server',
+  integrations: [react()],
 });
